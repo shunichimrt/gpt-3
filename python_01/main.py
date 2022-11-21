f1a1dd343f1332166668edd291e18e9f4da3b301
shunichimrt <smurata.official@gmail.com>
2022-11-21 11:25:04 +0900

    ファイル出力に変更

--- python_01/main.py
+++ python_01/main.py
@@ -1,19 +1,23 @@
 import sys
 import openai
+import datetime
 from openaienv import OpenAiEnv
 
+dt_now = datetime.datetime.now()
 openai.api_key = OpenAiEnv.OPENAI_API_KEY
 
-def main(dots):
-    
+def main():
     text_data = open("./target.txt", "r")
     contents = text_data.read().replace('　',' ')
     text_data.close()
-    
-    prompt = f'{contents}\n以下の{dots}点でまとめる。'
+    prompt = f'{contents}\nこれを要約する。'
     result = text_summary(prompt)
-
     return result
+
+def fileOutput(summary):
+    f = open('./result'+str(dt_now.strftime('%Y%m%d_%H%M%S'))+'.txt', 'w')
+    f.write(summary)
+    f.close()
 
 def text_summary(prompt):
     response = openai.Completion.create(
@@ -25,12 +29,9 @@ def text_summary(prompt):
         frequency_penalty=0.0,
         presence_penalty=0.0,
     )
-
     return response["choices"][0]["text"].replace('\n','')
 
 if __name__ == '__main__':
     args = sys.argv
-    dots = args[1]
-    
-    summary = main(dots)
-    print(summary)
+    summary = main()
+    fileOutput(summary)
